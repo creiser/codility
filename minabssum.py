ba160508c844c89ba93c5fc353d05fe5c7823523
Christian Reiser <christianreiser@gmx.net>
2017-03-19 23:18:53 +0100

    fast3 oh yeah baby

--- minabssum.py
+++ minabssum.py
@@ -1,10 +1,10 @@
-from itertools import *
-from sys import maxsize
+import itertools
+from sys import maxsize, setrecursionlimit
 import random
 
 def slow(A):
     res = maxsize
-    for comb in product(*[[-1, 1]] * len(A)):
+    for comb in itertools.product(*[[-1, 1]] * len(A)):
         temp = abs(sum([x * y for x, y in zip(A, comb)]))
         #print(temp)
         res = min(res, temp)
@@ -24,6 +24,7 @@ def step(current_sum, dp, possibs):
 def fast(A):
     A = sorted([abs(x) for x in A], reverse=True)
     total = sum(A)
+    max_elem = A[0]
     dp = [False] * (total + 1)
     return step(total, dp, A)
 
@@ -54,18 +55,73 @@ def fast(A):
         #print(unique_count)
         return res
 
+def step2(current_sum, count, dp):
+    res = maxsize
+    for i in reversed(range(len(count))):
+        if count[i] >= 1:
+            new = current_sum - 2 * i
+            if new >= 0 and dp[new] is False:
+                dp[new] = True
+                count[i] -= 1
+                res = min(res, new, step2(new, count, dp))
+                count[i] += 1
+    return res
 
+def fast2(A):
+    A = [abs(x) for x in A]
+    count = [0] * (max(A) + 1)
+    total = 0
+    for x in A:
+        total += x
+        count[x] += 1
+    dp = [False] * (total + 1)
+    return step2(total, count, dp)
+
+def fast3(A):
+    A = [abs(x) for x in A]
+    count = [0] * (max(A) + 1)
+    total = 0
+    for x in A:
+        total += x
+        count[x] += 1
+    dp = [False] * (total + 1)
+    res = maxsize
+
+    stack = [(total, -1)]
+    while stack:
+        current_sum, to_remove = stack.pop()
+        if current_sum != -1: # code for finish of depth traversal
+            if to_remove != -1:
+                count[to_remove] -= 1
+            for i in range(len(count)):
+                if count[i] >= 1:
+                    new = current_sum - 2 * i
+                    if new >= 0 and dp[new] is False:
+                        dp[new] = True
+                        res = min(res, new)
+                        if res == 0:
+                            return res
+                        stack.append((-1, i))
+                        stack.append((new, i))
+        elif to_remove != -1:
+            count[to_remove] += 1
+    return res
 
 def test():
     random.seed(a=43)
 
-    for t in range(10**5):
+    setrecursionlimit(30000)
+
+    for t in range(10**2):
         val = [random.randint(-100, 100) for i in range(5)]
-        #print(val)
-        s_res, f_res = slow(val), fast(val)
+        print(val)
+        #print(fast3(val))
+        s_res, f_res, f2_res, f3_res = slow(val), fast(val), fast2(val), fast3(val)
         print('slow: ' + str(s_res))
         print('fast: ' + str(f_res))
-        if s_res != f_res:
+        print('fast2: ' + str(f2_res))
+        print('fast3: ' + str(f3_res))
+        if s_res != f_res or s_res != f2_res or s_res != f3_res:
             print('solution false')
             break
 
